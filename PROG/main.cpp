c5c8fcfe70c1adae69e5549c6223d64bee25e4e0
Mateus Pedroza <mateuspedroza@mbp-de-mateus.lan>
2017-04-14 00:30:49 +0100

    Initial Commit

---
+++ PROG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PROG
+//
+//  Created by Mateus Pedroza on 14/04/17.
+//  Copyright © 2017 Mateus Pedroza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
