65a502cef27763151c9e46a3235fdcf6326d345a
Mateus Pedroza <mateuspedroza@MBP-de-Mateus.lan>
2017-04-14 18:56:30 +0100

    Deleted xcode proj

--- PROG/gui/main.cpp
+++ PROG/gui/main.cpp
@@ -6,44 +6,44 @@
 //  Copyright © 2017 Mateus Pedroza. All rights reserved.
 //
 
-#include "FileService.hpp"
-#include "Transportadora.hpp"
+#include "../logic/Transportadora.hpp"
+#include "../logic/FileService.hpp"
 
 int main(int argc, const char * argv[]) {
-    
+
     //  abrir ficheiro de linhas
     ifstream ficheiroLinhas;
     ficheiroLinhas.open("linhas.txt");
-    
+
     //  ver se ficheiro esta aberto
     if (ficheiroLinhas.is_open()) {
         // loop de linhas do ficheiro
         while (!ficheiroLinhas.eof()) {
             string linha;
-            
+
             getline(ficheiroLinhas, linha);
-            
+
             cout << linha << endl;
         }
     }
     ficheiroLinhas.close();
-    
-    
+
+
     //  abrir ficheiro de condutores
     ifstream ficheiroCondutores;
     ficheiroCondutores.open("condutores.txt");
-    
+
     //  ver se ficheiro esta aberto
     if (ficheiroCondutores.is_open()) {
         // loop de linhas do ficheiro
         while (!ficheiroCondutores.eof()) {
             string linha;
-            
+
             getline(ficheiroCondutores, linha);
-            
+
             cout << linha << endl;
         }
     }
     ficheiroCondutores.close();
-    
+
 }
