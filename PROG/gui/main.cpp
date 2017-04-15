08703eff24b9d845d1a067c3bda61af68bad0a0c
Mateus Pedroza <mpcmarques@icloud.com>
2017-04-15 01:05:31 +0100

    main

--- PROG/gui/main.cpp
+++ PROG/gui/main.cpp
@@ -6,8 +6,8 @@
 //  Copyright © 2017 Mateus Pedroza. All rights reserved.
 //
 
-#include "../LOGIC/Transportadora.hpp"
-#include "../LOGIC/FileService.hpp"
+#include "../logic/Transportadora.hpp"
+#include "../logic/FileService.hpp"
 
 int main(int argc, const char * argv[]) {
   vector<Linha> linhas = FileService().getLinhas();
